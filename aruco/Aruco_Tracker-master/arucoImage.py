3ddbdd35187ec728ef38a9ee1e5f13c29937dc11
Daniel Ionita <di118@live.mdx.ac.uk>
2019-04-14 14:01:28 +0100

    Update arucoImage.py

--- aruco/Aruco_Tracker-master/arucoImage.py
+++ aruco/Aruco_Tracker-master/arucoImage.py
@@ -1,107 +1,64 @@
 import numpy as np
-import cv2
-import cv2.aruco as aruco
-import glob
+import cv2, PIL
+from cv2 import aruco
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
 
-cap = cv2.imread('imageTest.jpg')
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
 
+fig = plt.figure()
+nx = 4
+ny = 3
+for i in range(1, nx*ny+1):
+    ax = fig.add_subplot(ny,nx, i)
+    img = aruco.drawMarker(aruco_dict,i, 700)
+    plt.imshow(img, cmap = mpl.cm.gray, interpolation = "nearest")
+    ax.axis("off")
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+plt.savefig("_data/markers.pdf")
+plt.show()
+frame = cv2.imread("_data/aruco_photo.jpg")
+plt.figure()
+plt.imshow(frame)
+plt.show()
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+parameters =  aruco.DetectorParameters_create()
+corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+plt.figure()
+plt.imshow(frame_markers)
+for i in range(len(ids)):
+    c = corners[i][0]
+    plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "o", label = "id={0}".format(ids[i]))
+plt.legend()
+plt.show()
+def quad_area(data):
+    l = data.shape[0]//2
+    corners = data[["c1", "c2", "c3", "c4"]].values.reshape(l, 2,4)
+    c1 = corners[:, :, 0]
+    c2 = corners[:, :, 1]
+    c3 = corners[:, :, 2]
+    c4 = corners[:, :, 3]
+    e1 = c2-c1
+    e2 = c3-c2
+    e3 = c4-c3
+    e4 = c1-c4
+    a = -.5 * (np.cross(-e1, e2, axis = 1) + np.cross(-e3, e4, axis = 1))
+    return a
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+corners2 = np.array([c[0] for c in corners])
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+data = pd.DataFrame({"x": corners2[:,:,0].flatten(), "y": corners2[:,:,1].flatten()},
+                   index = pd.MultiIndex.from_product(
+                           [ids.flatten(), ["c{0}".format(i )for i in np.arange(4)+1]],
+                       names = ["marker", ""] ))
 
-images = glob.glob('calib_images/*.jpg')
-
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
-
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
-
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners2)
-
-        # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-
-
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-
-
-while (True):
-    ret, frame =  cv2.imread('imageTest.jpg')
-    # operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-    parameters = aruco.DetectorParameters_create()
-
-    #lists of ids and the corners beloning to each id
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-
-
-    font = cv2.FONT_HERSHEY_SIMPLEX #font for displaying text (below)
-
-
-    if np.all(ids != None):
-
-        rvec, tvec ,_ = aruco.estimatePoseSingleMarkers(corners, 0.05, mtx, dist) #Estimate pose of each marker and return the values rvet and tvec---different from camera coefficients
-        #(rvec-tvec).any() # get rid of that nasty numpy value array error
-
-        for i in range(0, ids.size):
-            aruco.drawAxis(frame, mtx, dist, rvec[i], tvec[i], 0.1)  # Draw Axis
-            x = str(int((corners[i - 1][0][0][0] + corners[i - 1][0][1][0] + corners[i - 1][0][2][0] + corners[i - 1][0][3][
-                0]) / 4))
-            y = str(int((corners[i - 1][0][0][1] + corners[i - 1][0][1][1] + corners[i - 1][0][2][1] + corners[i - 1][0][3][
-                1]) / 4))
-        rotM = np.zeros(shape=(3, 3))
-        angle = str(cv2.Rodrigues(rvec[i - 1], rotM, jacobian=0))
-        aruco.drawDetectedMarkers(frame, corners) #Draw A square around the markers
-
-
-
-        ###### DRAW ID #####
-        strg = ''
-        for i in range(0, ids.size):
-            if ids[i] == 2:
-                strg += 'glasses, '
-                print("glasses")
-            elif ids[i] == 1:
-                strg += 'lamp, '
-                print("lamp")
-            elif ids[i] == 3:
-                strg += 'calender, '
-                print("calendar")
-            else:
-                strg += str(ids[i][0])+', '
-
-
-
-
-
-        cv2.putText(frame, ("Id: " + strg + "(" + x + "," + y + ")" ), (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
-
-
-    else:
-        ##### DRAW "NO IDS" #####
-        cv2.putText(frame, "No Ids", (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
-
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+data = data.unstack().swaplevel(0, 1, axis = 1).stack()
+data["m1"] = data[["c1", "c2"]].mean(axis = 1)
+data["m2"] = data[["c2", "c3"]].mean(axis = 1)
+data["m3"] = data[["c3", "c4"]].mean(axis = 1)
+data["m4"] = data[["c4", "c1"]].mean(axis = 1)
+data["o"] = data[["m1", "m2", "m3", "m4"]].mean(axis = 1)
+data
